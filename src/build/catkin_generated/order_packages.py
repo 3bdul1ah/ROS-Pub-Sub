2b9245e2bb785d2fe0c479909a3876318331c4d9
3bdu1lah <a2053231@gmail.com>
2024-09-26 10:17:22 +0800

    opencv

--- src/build/catkin_generated/order_packages.py
+++
@@ -1,5 +0,0 @@
-# generated from catkin/cmake/template/order_packages.context.py.in
-source_root_dir = '/home/abdullah/catkin_worksoace_1/src'
-whitelisted_packages = ''.split(';') if '' != '' else []
-blacklisted_packages = ''.split(';') if '' != '' else []
-underlay_workspaces = '/home/abdullah/catkin_worksoace_1/devel;/opt/ros/noetic'.split(';') if '/home/abdullah/catkin_worksoace_1/devel;/opt/ros/noetic' != '' else []
--- src/build/pubsub_code/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/abdullah/catkin_worksoace_1/src/pubsub_code
-# Build directory: /home/abdullah/catkin_worksoace_1/src/build/pubsub_code
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
